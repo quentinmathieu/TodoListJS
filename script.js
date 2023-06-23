a2be6f64fb9404ba9d082a8f8c7c1129d2140e50
quentinmathieu <quentin.mth@gmail.com>
2023-06-23 11:55:11 +0200

    Checking const in JS and click event listener

---
+++ script.js
@@ -0,0 +1,12 @@
+const addBtn = document.querySelector('#btn');
+const tasks = document.querySelectorAll(".task");
+const tasksContainer= document.querySelector("#todocards");
+addBtn.addEventListener('click', addTask);
+
+function addTask(){
+    console.log(addBtn)
+    console.log(tasks)
+    console.log(tasksContainer)
+    
+
+}
