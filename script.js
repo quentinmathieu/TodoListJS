c0ca6048b01c37674c00da38df10f73e97608746
quentinmathieu <quentin.mth@gmail.com>
2023-06-27 13:47:21 +0200

    updateCount() added (count todo cards)

--- script.js
+++ script.js
@@ -3,25 +3,36 @@ const taskCard = document.querySelector(".todoCard");
 const tasksContainer= document.querySelector("#todoCards");
 const delBtn = document.querySelector('.delBtn');
 
+updateCount();
 
-addBtn.addEventListener('click', addTask);
-delBtn.addEventListener('click', function() {
-    deleteTask(taskCard);
+addBtn.addEventListener('click', addTask); // add a task on click
+delBtn.addEventListener('click', function() { // delete a task on click
+    deleteTask(taskCard); // target the right task
   });
-
+  
 function addTask(){
-    const newTask = taskCard.cloneNode(true)
+    const newTask = taskCard.cloneNode(true) // clone the task card
     const newDelBtn = newTask.querySelector('.delBtn')
     const newTextArea = newTask.querySelector('.task')
 
-    newTextArea.value = "New Task"
-    newDelBtn.addEventListener('click', function() {
-        deleteTask(newTask);
+    newTextArea.value = "New Task" // set new task text to "New Task"
+    newDelBtn.addEventListener('click', function() { // add delete event listener to new task
+        deleteTask(newTask); // target the new task
     });
 
-    tasksContainer.appendChild(newTask) 
+    tasksContainer.appendChild(newTask) // append new task to the tasks container
+    updateCount();
 }
 
 function deleteTask(task){
-    task.remove();
+    task.remove(); // remove the task
+    updateCount();
 }
+
+function updateCount(){
+    const tasks = document.querySelectorAll('.todoCard');
+    const count = document.querySelector('#count');
+    count.textContent = tasks.length;
+    console.log(tasks.length);
+}
+
